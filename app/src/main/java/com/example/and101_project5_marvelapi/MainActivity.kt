05e59b248e753467f99a8337f8e662ff0fdd14dc
Landin Kasti <lkastiman@gmail.com>
2023-03-31 12:55:10 -0500

    finished

--- app/src/main/java/com/example/and101_project5_marvelapi/MainActivity.kt
+++ app/src/main/java/com/example/and101_project5_marvelapi/MainActivity.kt
@@ -1,23 +1,40 @@
 package com.example.and101_project5_marvelapi
 
-import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-import com.codepath.asynchttpclient.AsyncHttpClient
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.support.annotation.Nullable
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
-import org.w3c.dom.Text
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.*
 import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
     private var imageUrl : String = ""
+    private var nameText : String = ""
+    private var descText : String = ""
+    private var rand : Int = 0
+
+    @GlideModule
+    class MyAppGlideModule : AppGlideModule() {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,27 +46,51 @@ class MainActivity : AppCompatActivity() {
         val image = findViewById<ImageView>(R.id.image1)
 
         button.setOnClickListener{
-            getResponse(name, description)
-            getImage(image)
+                getResponse()
+
+                setImage(image)
+                name.text = nameText
+                description.text = descText
+
         }
 
 
     }
 
-    private fun getImage(imageView: ImageView){
+    private fun setImage(imageView: ImageView){
         Glide.with(this)
             .load(imageUrl)
             .fitCenter()
+            .override(300, 300)
+            .listener(object : RequestListener<Drawable?> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable?>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.d("glide exception:", "$e")
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable?>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+            })
             .into(imageView)
     }
 
-    private fun getResponse(nameView : TextView, descriptionView : TextView){
-        val client = AsyncHttpClient()
-        val hash = "1214aba0de6f1fb91de25c25ffd66d50"
-        val public_key = "16945dc672c2cb9628d201d80acbff33"
-        val ts = "1"
 
-        client["https://gateway.marvel.com/v1/public/characters?ts=1&apikey=16945dc672c2cb9628d201d80acbff33&hash=1214aba0de6f1fb91de25c25ffd66d50&limit=50",
+    private fun getResponse(){
+        val client = AsyncHttpClient()
+
+        client["https://api.jikan.moe/v4/random/characters",
             object : JsonHttpResponseHandler(){
                 override fun onFailure(
                     statusCode: Int,
@@ -60,28 +101,18 @@ class MainActivity : AppCompatActivity() {
                     Log.d("Fail", response.toString())
                 }
 
-                override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
-                    val listCharacterObjects = json?.jsonObject?.getJSONObject("data")?.getJSONArray("results")
+                override fun onSuccess(statusCode: Int, headers: Headers?, json: JsonHttpResponseHandler.JSON) {
+                    imageUrl = json.jsonObject.getJSONObject("data").getJSONObject("images").getJSONObject("jpg").getString("image_url")
+                    nameText = json.jsonObject.getJSONObject("data").getString("name")
 
-                    val rand = listCharacterObjects?.let { Random.nextInt(0, it.length()-1) }
-                    var name : String
-                    var description : String
-                    var imageUrl : String
-
-                    if (rand != null) {
-                        name = listCharacterObjects?.getJSONObject(rand)?.getString("name") as String
-                        description = listCharacterObjects?.getJSONObject(rand)?.getString("description") as String
-                        imageUrl = listCharacterObjects?.getJSONObject(rand)?.getJSONObject("thumbnail")?.getString("path") + ".jpg"
-
-
-                        nameView.text = name
-                        descriptionView.text = description
-                    }
-
-
-
-
+                    descText =
+                        if(json.jsonObject.getJSONObject("data").getString("about") == "") {
+                            json.jsonObject.getJSONObject("data").getString("name_kanji")
+                        } else {
+                            json.jsonObject.getJSONObject("data").getString("about")
+                        }
                 }
             }]
+
     }
 }
